d4da58c185121d9a46b1d4d1b7cc9e3d0f3c5df0
AlexandrNeverov <nev.al.vic1@gmail.com>
2025-01-16 18:52:58 -0500

    7

--- main.tf
+++ main.tf
@@ -20,21 +20,20 @@ terraform {
     }
 }
 
-variable "aws_acess_key" {
-    description = "AWS Access Key ID"
-    type = string
-    sensitive = true
-  
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type = string
-  sensitive = true
-}
-
 provider "aws" {
-    region = "us-east-1"
-    access_key = "aws_acess_key"
-    secret_key = "aws_secret_key"
+  region     = "us-east-1"          
+  access_key = "YOUR_ACCESS_KEY"      
+  secret_key = "YOUR_SECRET_KEY"     
+}
+
+resource "aws_key_pair" "MKP" {
+    key_name = "MKP"
+    public_key = file ("/home/ubuntu/infrapro/InfraPro")
+}
+
+resource "aws_instance" "dev_infrapro" {
+    ami = "ami-04b4f1a9cf54c11d0"
+    instance_type = "t2.micro"
+    key_name = "MKP"
+  
 }
